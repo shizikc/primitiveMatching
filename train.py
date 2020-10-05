8758745ea8cd63a9303144db4f4da7cfbaa7fc84
sharoncoo <shizik610@gmail.com>
2020-10-05 14:28:23 +0300

    turn validation off

--- train.py
+++ train.py
@@ -78,10 +78,10 @@ def fit(epochs, model, loss_obj, opt, train_dl, valid_dl):
         # logging.info("Epoch (Valid): {:3d}, total loss : {:05.4f}".format(epoch, val_loss))
         # TODO: when turning validation - replace minimum loss with val_loss
         if epoch == reg_start_iter:
-            min_loss = loss_obj.temp_metrics.total_loss
+            min_loss = loss_obj.temp_metrics['total_loss']
 
-        if epoch >= reg_start_iter and loss_obj.temp_metrics.total_loss <= min_loss:
-            min_loss = loss_obj.temp_metrics.total_loss
+        if epoch >= reg_start_iter and loss_obj.temp_metrics['total_loss'] <= min_loss:
+            min_loss = loss_obj.temp_metrics['total_loss']
 
             # save minimum model
             torch.save(model.state_dict(), model_path)
